acd1f1be4a003b22405932344b40ec27c5d926a2
DavideAtzo <atzo9477@gmail.com>
2023-03-07 15:12:59 +0100

    console log membri

--- js/script.js
+++ js/script.js
@@ -8,37 +8,36 @@
 // Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 
 const team = [
-    {
+    [{
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
         photo: 'wayne-barnett-founder-ceo.jpg',
-    },
-    {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO',
-        photo: 'wayne-barnett-founder-ceo.jpg',
-    },
-    {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO',
-        photo: 'wayne-barnett-founder-ceo.jpg',
-    },
-    {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO',
-        photo: 'wayne-barnett-founder-ceo.jpg',
-    },
-    {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO',
-        photo: 'wayne-barnett-founder-ceo.jpg',
-    },
-    {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO',
-        photo: 'wayne-barnett-founder-ceo.jpg',
-    }
+    }],
+    [{
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg',
+    }],
+    [{
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg',
+    }],
+    [{
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg',
+    }],
+   [ {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg',
+    }],
+    [{
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg',
+    }],
 ]
 
-
-console.log({team})
+console.log({team});
